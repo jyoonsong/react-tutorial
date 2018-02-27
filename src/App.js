20bd93649e1a2a79d82717d7219964576077f8b2
jaeyo_on <jyo3on@gmail.com>
2018-02-27 23:23:58 +0900

    new create-react-app template

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
 import React, { Component } from 'react';
-import UserList from './components/UserList';
-import { Map, List } from 'immutable';
-
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
-	id = 3 // 이미 0,1,2가 존재
-	
-	state = {
-		data: Map({
-			input: '',
-			users: List([
-				Map({ id: 1, username: 'jaeyoon' }),
-				Map({ id: 2, username: 'song' })
-			])
-		})
-	}
-
-	onChange = (e) => {
-		const {value} = e.target;
-		const {data} = this.state;
-
-		this.setState({
-			data: data.set('input', value)
-		});
-	}
-
-	onButtonClick = (e) => {
-		const {data} = this.state;
-
-		this.setState({
-			data: data.set('input', '') // input: ''
-								.update('users', users => users.push( // users.concat 새 Map을 추가
-									Map({
-										id: this.id++,
-										username: data.get('input') // data.input은 불가능
-									})
-								))
-		})
-	}
-
   render() {
-  	// this 생략을 위해
-  	const {data} = this.state;
-  	const input = data.get('input');
-  	const users = data.get('users');
-  	const {onChange, onButtonClick} = this;
-
     return (
-			<div>
-				<div>
-					<input onChange={onChange} value={input} />
-					<button onClick={onButtonClick}>추가</button>
-				</div>
-				<h3>사용자목록</h3>
-				<UserList users={users} />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
