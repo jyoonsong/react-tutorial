8b0a4cb2ee5df87df34d69494cde3ed9f6205b83
jaeyo_on <jyo3on@gmail.com>
2018-02-27 21:27:35 +0900

    tmp

---
+++ src/components/UserList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import User from './User';
+
+export default class UserList extends Component {
+	renderUsers = () => {
+		const {users} = this.props;
+		return users.map(user =>
+			<User key={user.id} user={user} />
+		);
+	}	
+
+	render() {
+		console.log('Rendering UserList');
+		const {renderUsers} = this;
+		return (
+			<div>
+				{renderUsers()}
+			</div>
+		);
+	}
+}
