72ad306e8725676c29bfe718ee9fb472af81947d
jaeyo_on <jyo3on@gmail.com>
2018-03-01 22:14:59 +0900

    empty prev codes

--- src/lib/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export function getAPOD(date='') {
-  return axios.get(`https://api.nasa.gov/planetary/apod?api_key=QfhuH8V4pducM9egumUUG90I9LbHwDLKovPgODFt&date=${date}`);
-}
